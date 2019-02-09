af543cf198a750c5793000bf6afb4cb42563c5eb
James Cox <jamesncox@yahoo.com>
2019-02-09 20:49:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,8 @@
 # lib/introduction.RUBY_VERSION
 
 def introduction(name)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name}."
+  
+def #introduction_with_language
+  puts "Hi, my name is #{name} and Iam learning to program in #{language}."
 end
